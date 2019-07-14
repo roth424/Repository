f6ba8c4ab9a00ff9cf82eee1ff3bb59044840ab3
Justin Roth <rothjusm@gmail.com>
2019-07-14 13:32:51 -0500

    WeatherPy Homework

---
+++ WeatherPy/config.py
@@ -0,0 +1,9 @@
+nyt_api_key = "mJuqYqXY8MlwICPQUXmWuEnnwZHCndGj"
+
+omdb_api_key = "YOUR KEY HERE"
+
+weather_key="ba7a88ebabad4f8099210d8975fc107f"
+
+gkey="AIzaSyAS3E18OuWYswbM-PSzT0zoS3G6OF3QcME"
+
+census_key = "90a3b4413de14a5a52626999ba58589f7a37f90d"
